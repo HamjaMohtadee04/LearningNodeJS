4fbf93693a5f8df8aa222ce1b7ce018a0d28ceae
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-05-31 16:29:42 +0600

    sets route'

--- module2/ToDO-App/server.js
+++ module2/ToDO-App/server.js
@@ -1,8 +1,19 @@
 const http = require("http")
 
 const server = http.createServer((req,res)=>{
-    console.log({req,res});
-    res.end("welcome to ToDo app server")
+    // console.log({req,res});
+    // console.log(req.url,req.method);
+    // res.end("welcome to ToDo app server")
+
+
+    if (req.url ==="/todos" && req.method ==='GET'){
+        res.end("All todos")
+    } else if(req.url ==="/todos/create-todo" && req.method ==='POST'){
+        res.end("todo created")
+    } else{
+        res.end("route not found")
+    }
+
 })
 
 server.listen(5000,"127.0.0.1",()=>{
