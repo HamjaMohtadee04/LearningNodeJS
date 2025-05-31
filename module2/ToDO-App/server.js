00c3ae1b7887ec5427bd4810553355e3b1535866
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-05-31 19:58:04 +0600

    single todo code added

--- module2/ToDO-App/server.js
+++ module2/ToDO-App/server.js
@@ -1,51 +1,65 @@
 const http = require("http")
-const { text, json } = require("stream/consumers")
-
-const data =[
-  {
-    "title": "Buy groceries",
-    "body": "Milk, eggs, bread, and vegetables",
-    "createdAt": "2025-05-31T10:30:00Z"
-  },
-  {
-    "title": "Study TypeScript",
-    "body": "Review union types and generics for 1 hour",
-    "createdAt": "2025-05-31T12:00:00Z"
-  },
-  {
-    "title": "Fix UI bug in dashboard",
-    "body": "Resolve alignment issue in the header component",
-    "createdAt": "2025-05-31T14:15:00Z"
-  },
-  {
-    "title": "Write blog post",
-    "body": "Topic: Introduction to Manual Testing for Beginners",
-    "createdAt": "2025-05-31T15:45:00Z"
-  }
-]
+// const { text, json } = require("stream/consumers")
+const path = require("path")
+const filePath =path.join(__dirname,"./db/todo.json")
+const fs = require("fs")
 
 
 const server = http.createServer((req,res)=>{
-    // console.log({req,res});
-    // console.log(req.url,req.method);
-    // res.end("welcome to ToDo app server")
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = url.pathname
+
 
 //setting headers
-    if (req.url ==="/todos" && req.method ==='GET'){
+//get all todos
+    if (pathname ==="/todos" && req.method ==='GET'){
+
+        const data = fs.readFileSync(filePath,{encoding:"utf-8"})
+
         res.writeHead(200,{
-            // "content-type":"application/json",
-            "content-type":"text/html",
-            // "gmail":'todo@gmail.com'
+            "content-type":"application/json",
+        })
+        res.end(
+            data
+        )
+    } 
+    //post a todo
+    else if(pathname ==="/todos/create-todo" && req.method ==='POST'){
+        let data =""
+        req.on("data",(chunk)=>{
+            data =data + chunk
         })
 
-        // res.setHeader("content-type","text/plain")
-        // res.setHeader("email","tod01@gmail.com")
-        // res.statusCode =201
-        // res.end(JSON.stringify(data))
-        res.end(`<h1>hello there</h1> <h1>hello there</h1> <h1>hello there</h1>`)
-    } else if(req.url ==="/todos/create-todo" && req.method ==='POST'){
-        res.end("todo created")
-    } else{
+        req.on("end",()=>{
+            const {title,body} =JSON.parse(data)
+            const createdAt = new Date().toLocaleString()
+
+            const allTodos = fs.readFileSync(filePath,{encoding:"utf-8"})
+            const parsedAlltodos = JSON.parse(allTodos)
+            
+             parsedAlltodos.push({title,body,createdAt})  
+             fs.writeFileSync(filePath,JSON.stringify(parsedAlltodos,null,2),{encoding:'utf-8'})
+              res.end(JSON.stringify(
+                {title,body,createdAt},null,2
+              ))
+        })
+ }
+
+    else if (pathname==="/todo" && req.method ==='GET'){
+       const title = url.searchParams.get("title")
+       console.log(title);
+     const data = fs.readFileSync(filePath,{encoding:"utf-8"})
+const parsedData = JSON.parse(data)
+const todo = parsedData.find((todo)=> todo.title === title)
+const stringifiedTodo = JSON.stringify(todo)
+        res.writeHead(200,{
+            "content-type":"application/json",
+        })
+        res.end(stringifiedTodo)
+        res.end("single todo")
+    }
+    
+    else{
         res.end("route not found")
     }
 
