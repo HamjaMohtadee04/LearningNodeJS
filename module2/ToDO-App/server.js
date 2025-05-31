168ac2c433893f86d8594b7588e4d6bf9f60ede0
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-05-31 16:13:29 +0600

    created server for todo app

---
+++ module2/ToDO-App/server.js
@@ -0,0 +1,11 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+    console.log({req,res});
+    res.end("welcome to ToDo app server")
+})
+
+server.listen(5000,"127.0.0.1",()=>{
+    console.log("server listening to localhost");
+})
+
